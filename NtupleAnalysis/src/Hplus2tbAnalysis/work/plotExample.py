dd98f15afba010068f65be4690ea4adfd8c8024b
Heitor Pascoal de Bittencourt <heitorpbittencourt@gmail.com>
2016-06-20 12:21:54 +0300

    improved plot script

--- NtupleAnalysis/src/Hplus2tbAnalysis/work/plotExample.py
+++ NtupleAnalysis/src/Hplus2tbAnalysis/work/plotExample.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import re
 
 import HiggsAnalysis.NtupleAnalysis.tools.dataset as dataset
 import HiggsAnalysis.NtupleAnalysis.tools.tdrstyle as tdrstyle
@@ -77,44 +78,53 @@ def plot(hName, title_x, rebin = 0):
 
     style = tdrstyle.TDRStyle()
 
-    dataset1 = datasetsHiggs.getDataset("ChargedHiggs_HplusTB_HplusToTauB_M_200").getDatasetRootHisto(hName)
-    dataset2 = datasetsTT.getDataset("MC").getDatasetRootHisto(hName)
-#    dataset1.normalizeToOne()
-    dataset2.normalizeToOne()
+    masses = datasetsHiggs.getAllDatasetNames()
 
-    histo1 = dataset1.getHistogram()
-    histo1.SetMarkerColor(2)
-    histo1.SetMarkerStyle(20)
-    removeNegatives(histo1)
-    histo1.Scale(1./histo1.Integral())
+    for m in masses:
+        m_re = re.compile("\S+_M_(?P<value>(\d+))$")
+        match = m_re.search(m)
+        m_str = match.group("value")
 
-    histo2 = dataset2.getHistogram()
-    histo2.SetMarkerColor(4)
-    histo2.SetMarkerStyle(21)
+        dataset1 = datasetsHiggs.getDataset(m).getDatasetRootHisto(hName)
+        dataset2 = datasetsTT.getDataset("MC").getDatasetRootHisto(hName)
+#        dataset1.normalizeToOne()
+        dataset2.normalizeToOne()
 
-    if rebin:
-        histo1.Rebin(rebin)
-        histo2.Rebin(rebin)
+        histo1 = dataset1.getHistogram()
+        histo1.SetMarkerColor(2)
+        histo1.SetMarkerStyle(20)
+        removeNegatives(histo1)
+        histo1.Scale(1./histo1.Integral())
 
-    p = plots.ComparisonPlot(histograms.Histo(histo1, "m_{H^{#pm}} = 200 GeV/c^{2}", "p", "P"),
-                             histograms.Histo(histo2, "t#bar{t}", "p", "P"))
+        histo2 = dataset2.getHistogram()
+        histo2.SetMarkerColor(4)
+        histo2.SetMarkerStyle(21)
 
-    opts = {"ymin": 0, "ymax": 0.2}
-    opts2 = {"ymin": 0.5, "ymax": 1.5}
-    p.createFrame(os.path.join(plotDir, plotname), createRatio=True, opts=opts, opts2=opts2)
+        if rebin:
+            histo1.Rebin(rebin)
+            histo2.Rebin(rebin)
 
-    moveLegend = {"dx": -0.2, "dy": -0.1, "dh": -0.1}
-    p.setLegend(histograms.moveLegend(histograms.createLegend(), **moveLegend))
+        p = plots.ComparisonPlot(histograms.Histo(histo1, "m_{H^{#pm}} = " + m_str + " GeV/c^{2}", "p", "P"),
+                                 histograms.Histo(histo2, "t#bar{t}", "p", "P"))
 
-    p.getFrame().GetYaxis().SetTitle("Arbitrary units")
-    p.getFrame().GetXaxis().SetTitle(title_x)
-    p.getFrame2().GetYaxis().SetTitle("Ratio")
-    p.getFrame2().GetYaxis().SetTitleOffset(1.6)
+        opts = {"ymin": 0, "ymax": 0.2}
+        opts2 = {"ymin": 0.5, "ymax": 1.5}
+        p.createFrame(os.path.join(plotDir + "/" + m_str, plotname), createRatio=True, opts=opts, opts2=opts2)
 
-    p.draw()
-    if not os.path.exists(plotDir):
-        os.mkdir(plotDir)
-    p.save(formats)
+        moveLegend = {"dx": -0.2, "dy": -0.1, "dh": -0.1}
+        p.setLegend(histograms.moveLegend(histograms.createLegend(), **moveLegend))
+
+        p.getFrame().GetYaxis().SetTitle("Arbitrary units")
+        p.getFrame().GetXaxis().SetTitle(title_x)
+        p.getFrame2().GetYaxis().SetTitle("Ratio")
+        p.getFrame2().GetYaxis().SetTitleOffset(1.6)
+
+        p.draw()
+        if not os.path.exists(plotDir):
+            os.mkdir(plotDir)
+        if not os.path.exists(plotDir + "/" + m_str):
+            os.mkdir(plotDir + "/" + m_str)
+        p.save(formats)
 
 if __name__ == "__main__":
     main()
